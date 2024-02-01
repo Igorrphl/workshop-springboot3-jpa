4362ba821228f06f9eedb0160e7dd14fc86e2776
Igor Rodrigues <igrrph@gmail.com>
2024-02-01 20:34:55 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -0,0 +1,14 @@
+package com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Configura o projeto para que ele seja uma aplicação do springBoot
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
