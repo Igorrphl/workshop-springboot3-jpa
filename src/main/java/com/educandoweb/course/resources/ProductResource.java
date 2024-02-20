c23bdd194d35ab705f07a52b140c6361692b53b5
Igor Rodrigues <igrrph@gmail.com>
2024-02-20 20:34:44 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -0,0 +1,40 @@
+package com.educandoweb.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+	@Autowired
+	private ProductService service;
+
+	// responde a uma requisição do tipo Get do http
+	@GetMapping
+	// endpoint para acessar os usuários
+	// ReponseEntity - Retorna resposta de requisições web
+	public ResponseEntity<List<Product>> findAll() {
+
+		List<Product> list = service.findAll();
+
+		// .ok - retornando a resposta com sucesso
+		// .body - retornando o corpo da resposta
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
